ec8ba558e38fa54a130c7fdfff2d70c1d15100e4
harungo <1reckoner1@gmail.com>
2021-11-29 16:30:59 +0300

    Added optimization compiler flag
    Added optimization compiler flag -o:size for smaller binaries

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-odin build src -out:app.wasm -target:freestanding_wasm32 -no-entry-point -extra-linker-flags:"-zstack-size=2048 --max-memory=65536 --strip-all"
+odin build src -out:app.wasm -target:freestanding_wasm32 -o:size -no-entry-point -extra-linker-flags:"-zstack-size=2048 --max-memory=65536 --strip-all"
 
 xxd -i app.wasm > Wasm_Blink\app.wasm.h
